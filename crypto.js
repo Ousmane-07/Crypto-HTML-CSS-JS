cf83ff8213af5cb27733659e4f8c5722af7e7955
Ousmane <ousmanegitgit@gmail.com>
2026-04-07 02:05:47 -0400

    started over side-bar due to bug

--- crypto.js
+++
@@ -1,6 +0,0 @@
-const body = document.querySelector("body");
-const modeSwitch = document.querySelector(".toggle-switch");
-
-modeSwitch.addEventListener("click", () => { 
-    body.classList.toggle("dark");
-})
